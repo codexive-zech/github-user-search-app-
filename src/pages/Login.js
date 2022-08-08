be137d4f3e64b6c6f4d11aac9e912a0f9e936047
Codexive_zech <zechariahhounwanou@gmail.com>
2022-08-08 01:34:22 +0100

    Added starter files along side set up routing, Error page and Login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,18 @@
-import React from 'react';
-import { useAuth0 } from '@auth0/auth0-react';
-import styled from 'styled-components';
-import loginImg from '../images/login-img.svg';
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import styled from "styled-components";
+import loginImg from "../images/login-img.svg";
+import { Link } from "react-router-dom";
 const Login = () => {
-  return <h2>login page</h2>;
+  return (
+    <Wrapper>
+      <section className="container">
+        <img src={loginImg} alt="github user login" />
+        <h1>Github User</h1>
+        <Link className="btn">Login</Link>
+      </section>
+    </Wrapper>
+  );
 };
 const Wrapper = styled.section`
   min-height: 100vh;
